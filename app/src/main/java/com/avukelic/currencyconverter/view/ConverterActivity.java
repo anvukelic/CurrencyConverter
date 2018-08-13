6df07fe2519ed155a749d4f0835866153d4e2b30
Andrej Vukelić <avukelic031@gmail.com>
2018-08-13 22:27:13 +0200

    Method removed

--- app/src/main/java/com/avukelic/currencyconverter/view/ConverterActivity.java
+++ app/src/main/java/com/avukelic/currencyconverter/view/ConverterActivity.java
@@ -106,12 +106,4 @@ public class ConverterActivity extends AppCompatActivity implements ConverterCon
     public void showNetworkErrorMessage() {
         Toast.makeText(this, "You have some network issues", Toast.LENGTH_SHORT).show();
     }
-
-    public static void hideSoftKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager =
-                (InputMethodManager) activity.getSystemService(
-                        Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(
-                activity.getCurrentFocus().getWindowToken(), 0);
-    }
 }
